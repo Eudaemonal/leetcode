2fb9eec5835845aec4e12ecc10e4aa5ce1a476fd
eudaemonal <eudaemonal@gmail.com>
2018-12-05 16:55:30 +1100

    Add Longest Substring without repeating characters

---
+++ 003_Longest_Substring_Without_Repeating_Characters/test01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+
+
+
+int lengthOfLongestSubstring(std::string s){
+    std::unordered_map<char, int> m;
+    int max_len = 0, start = 0;
+    for(int i = 0; i < s.length(); ++i){
+        if(m.count(s[i]) && m[s[i]] >= start)
+            start = m[s[i]] + 1;
+        m[s[i]] = i;
+
+        max_len = std::max(max_len, i - start + 1);
+    }
+    return max_len;
+}
+
+
+int main(int argc, char *argv[]){
+    std::string s;
+    std::getline(std::cin, s);
+	
+	std::cout <<lengthOfLongestSubstring(s) << "\n";
+	return 0;
+}
