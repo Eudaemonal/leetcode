d0eadaf06f64c90670976a8ab9eff09ef1f5ae22
eudaemonal <eudaemonal@gmail.com>
2018-12-07 17:07:42 +1100

    Median of Two Sorted Array, non optimum solution updated

---
+++ 004_Median_of_Two_Sorted_Arrays/test01.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    os << "[" << p.first << ", " << p.second << "]";
+    return os;
+}
+
+template <template <typename, typename...> class ContainerType, 
+          typename ValueType, typename... Args>
+std::ostream& operator<<(std::ostream& os, const ContainerType<ValueType, Args...>& c){
+    for(const auto& v : c){
+        os << v << ' ';
+    }
+    os << '\n';
+    return os;
+}
+
+
+double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2){
+    int n = nums1.size();
+    int m = nums2.size();
+
+    std::vector<int> combined;
+    int i =0, j=0;
+    while(i < n && j < m){
+        if(nums1[i] < nums2[j]){
+            combined.push_back(nums1[i++]);
+        }else{
+            combined.push_back(nums2[j++]);
+        }
+    }
+    while(i < n){
+        combined.push_back(nums1[i++]);
+    }
+    while(j < m){
+        combined.push_back(nums2[j++]);
+    }
+
+    int size = combined.size();
+    if(size % 2){
+        return double(combined[size/2]);
+    }else{
+        return double(combined[size/2-1] + combined[size/2])/2;
+    }
+}
+
+int main(int argc, char *argv[]){
+    int n, m;
+    std::cin >> n;
+    std::vector<int> v1(n);
+    for(int i = 0; i < n; ++i){
+        std::cin >> v1[i];
+    }
+    std::cin >> m;
+    std::vector<int> v2(m);
+    for(int i = 0; i < m; ++i){
+        std::cin >> v2[i];
+    }
+
+    std::cout << findMedianSortedArrays(v1, v2) << "\n";
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 This repo contains my leetcode solutions in C++14
 
-Require CMake version >=3.2
+## Content
+001_Two_Sum                                             DONE  
+002_Add_Two_Numbers                                     Can be improved  
+003_Longest_Substring_Without_Repeating_Characters      *  
+004_Median_of_Two_Sorted_Arrays                         *  
+
+
+
+
 
 ## How to use
 
