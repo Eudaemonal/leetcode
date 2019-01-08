5e95e5c664e77c445ebb0f98f361b3aa503e9518
eudaemonal <eudaemonal@gmail.com>
2019-01-08 16:55:31 +1100

    Add Remove Nth Node From end of list

---
+++ 019_Remove_Nth_Node_From_End_of_List/test01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+ListNode* removeNthFromEnd(ListNode* head, int n){
+    
+}
+
+int main(int argc, char *argv[]){
+
+}
