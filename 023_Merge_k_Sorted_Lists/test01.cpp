cf2c2a0264c2b2c5df98e7b7afc850aa642be665
eudaemonal <eudaemonal@gmail.com>
2019-01-09 21:01:02 +1100

    Add Merge k sorted lists

--- 023_Merge_k_Sorted_Lists/test01.cpp
+++ 023_Merge_k_Sorted_Lists/test01.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 struct ListNode {
     int val;
@@ -16,6 +18,50 @@ std::ostream& operator<<(std::ostream& os, ListNode* curr){
     return os;
 }
 
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    for(const auto& c : v){
+        os << c;
+        os << " ";
+    }
+    os << "\n";
+    return os;
+}
+
+struct compare{
+    bool operator()(const ListNode* l, const ListNode* r){
+        return l->val > r->val;
+    }
+};
+
+ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+    std::priority_queue<ListNode*, std::vector<ListNode*>, compare> q;
+    for(auto list : lists){
+        if(list){
+            q.push(list);
+        }
+    }
+    if(q.empty()){
+        return nullptr;
+    }
+
+    ListNode* result = q.top();
+    q.pop();
+    if(result->next){
+        q.push(result->next);
+    }
+
+    ListNode* tail = result;
+    while(!q.empty()){
+        tail->next = q.top();
+        q.pop();
+        tail = tail->next;
+        if(tail->next){
+            q.push(tail->next);
+        }
+    }
+    return result;
+}
 
 ListNode* append(ListNode* head, int val){
     ListNode* node = new ListNode(val);
@@ -32,7 +78,20 @@ ListNode* append(ListNode* head, int val){
     return head;
 }
 
-
 int main(int argc, char *argv[]){
-    
+    int n, m, val;
+    std::vector<ListNode*> lists;
+
+    std::cin >> n;
+    for(int i = 0; i < n; ++i){
+        ListNode* list = nullptr;
+        std::cin >> m;
+        for(int j = 0; j < m; ++j){
+            std::cin >> val;
+            list = append(list, val);
+        }
+        lists.push_back(list);
+    }
+
+    std::cout << mergeKLists(lists);
 }
