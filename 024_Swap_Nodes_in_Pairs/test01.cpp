fe2616279ed9dd001fe0a6a5a84b6167580e8bec
eudaemonal <eudaemonal@gmail.com>
2019-01-10 21:39:40 +1100

    Add Swap Nodes in pairs

---
+++ 024_Swap_Nodes_in_Pairs/test01.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+std::ostream& operator<<(std::ostream& os, ListNode* curr){
+    while(curr){
+        os << curr->val;
+        os << " ";
+        curr = curr->next;
+    }
+    os << "\n";
+    return os;
+}
+
+
+
+ListNode* swapPairs(ListNode* head){
+    
+}
+
+
+ListNode* append(ListNode* head, int val){
+    ListNode* node = new ListNode(val);
+    if(head == nullptr){
+        head = node;
+    }
+    else{
+        ListNode* curr = head;
+        while(curr->next){
+            curr = curr->next;
+        }
+        curr->next = node;
+    }
+    return head;
+}
+
+int main(int argc, char *argv[]){
+    int n, val;
+    std::cin >> n;
+    ListNode* head = nullptr;
+    for(int i = 0; i < n; ++i){
+        std::cin >> val;
+        head = append(head, val);
+    }
+    std::cout << head;
+}
