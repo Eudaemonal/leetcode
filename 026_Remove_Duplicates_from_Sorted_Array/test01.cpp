6f13fef5b17258f3afd03da8c2829458afe4af39
eudaemonal <eudaemonal@gmail.com>
2019-01-12 17:00:06 +1100

    Add Divide Two Integers

--- 026_Remove_Duplicates_from_Sorted_Array/test01.cpp
+++ 026_Remove_Duplicates_from_Sorted_Array/test01.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 #include <vector>
 
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    os << "[" << p.first << ", " << p.second << "]";
+    return os;
+}
+
+template <template <typename, typename...> class ContainerType, 
+          typename ValueType, typename... Args>
+std::ostream& operator<<(std::ostream& os, const ContainerType<ValueType, Args...>& c){
+    for(const auto& v : c){
+        os << v << ' ';
+    }
+    os << '\n';
+    return os;
+}
+
+int removeDuplicates(std::vector<int>& nums) {
+    int n = nums.size();
+    if(n == 0){
+        return 0;
+    }
+    int i = 0;
+    for(int j = 1; j < n; ++j){
+        if(nums[j] != nums[i]){
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
+
 int main(int argc, char *argv[]){
-    
+    int n;
+    std::cin >> n;
+    std::vector<int> v(n);
+    for(int i = 0; i < n; ++i){
+        std::cin >> v[i];
+    }
+    std::cout << removeDuplicates(v) <<"\n";
 }
---
+++ 029_Divide_Two_Integers/test01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int divide(int dividend, int divisor) {
+    
+}
+
+int main(int argc, char *argv[]){
+    int dividend;
+    int divisor;
+
+    std::cin >> dividend;
+    std::cin >> divisor;
+
+    std::cout << divide(dividend, divisor) << "\n";
+}
