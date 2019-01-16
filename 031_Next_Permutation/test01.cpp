7799d5afff861490b82326f08850602ecb739b24
eudaemonal <eudaemonal@gmail.com>
2019-01-16 20:28:23 +1100

    Add next permutation

---
+++ 031_Next_Permutation/test01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    os << "[" << p.first << ", " << p.second << "]";
+    return os;
+}
+
+template <template <typename, typename...> class ContainerType, 
+          typename ValueType, typename... Args>
+std::ostream& operator<<(std::ostream& os, const ContainerType<ValueType, Args...>& c){
+    for(const auto& v : c){
+        os << v << ' ';
+    }
+    os << '\n';
+    return os;
+}
+
+void nextPermutation(vector<int>& nums) {
+    
+}
+
+int main(int argc, char *argv[]){
+    int n;
+    std::cin >> n;
+    std::vector<int> v(n);
+    for(int i = 0; i < n; ++i){
+        std::cin >> v[i];
+    }
+
+    std::cout << f(v) << "\n";
+    return 0;
+}
