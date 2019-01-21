1cd0067219daa288b46cec0b6e326c980078dbe7
eudaemonal <eudaemonal@gmail.com>
2019-01-21 13:26:04 +1100

    Add Longest Valid Parenthese

--- 032_Longest_Valid_Parentheses/test01.cpp
+++ 032_Longest_Valid_Parentheses/test01.cpp
@@ -17,25 +17,12 @@ std::ostream& operator<<(std::ostream& os, const ContainerType<ValueType, Args..
     return os;
 }
 
-void nextPermutation(std::vector<int>& nums) {
-    int n = nums.size(), k, l;
-    for(k = n-2; k>=0; k--){
-        if(nums[k] < nums[k+1]){
-            break;
-        }
+int f(std::vector<int> v){
+    int n = v.size();
+    for(int i = 0; i < n; ++i){
+        
     }
-    if(k < 0){
-        std::reverse(nums.begin(), nums.end());
-    }
-    else{
-        for(l = n-1; l > k; l--){
-            if(nums[l] > nums[k]){
-                break;
-            }
-        }
-        std::swap(nums[k], nums[l]);
-        std::reverse(nums.begin() + k + 1, nums.end());
-    }
+    return 0;
 }
 
 int main(int argc, char *argv[]){
@@ -46,8 +33,6 @@ int main(int argc, char *argv[]){
         std::cin >> v[i];
     }
 
-    nextPermutation(v);
-
-    std::cout << v;
+    std::cout << f(v) << "\n";
     return 0;
 }
