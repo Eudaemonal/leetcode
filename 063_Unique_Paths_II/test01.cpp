8cbcac4bd3c689317b0d5c1156aa837ac54e9f8f
eudaemonal <eudaemonal@gmail.com>
2019-01-28 17:35:20 +1100

    Add unique path 2

---
+++ 063_Unique_Paths_II/test01.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>> &v){
+	for(size_t i=0; i < v.size(); ++i){
+		for(size_t j=0; j < v[0].size(); ++j){
+			os << v[i][j] << " ";
+		}
+		os << "\n";
+	}
+	return os;
+}
+
+int uniquePathsWithObstacles(std::vector<std::vector<int>>& obstacleGrid) {
+    
+}
+
+int main(int argc, char *argv[]){
+	int n, m;
+	std::cin >> n >> m;
+	std::vector<std::vector<int>> v(n, std::vector<int>(m));
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < m; ++j){
+			std::cin >> v[i][j];
+		}
+	}
+	
+	std::cout << uniquePathsWithObstacles(v) << "\n";
+	return 0;
+}
