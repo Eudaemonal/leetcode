f55470b5a16c444810ae21cb621928aaf0155e22
eudaemonal <eudaemonal@gmail.com>
2019-02-21 14:08:58 +1100

    Add balance binary tree

--- 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/test01.cpp
+++ 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/test01.cpp
@@ -10,28 +10,29 @@ struct TreeNode {
 };
 
 std::ostream& operator<<(std::ostream& os, TreeNode *root)  {  
-    if (root == NULL) return os;  
-    std::queue<TreeNode*> q; 
+    if (root == NULL) return os;
+    std::queue<TreeNode*> q;
  
     q.push(root);
-    while (q.empty() == false)  {  
-        int count = q.size();  
-        while (count > 0) {  
-            TreeNode* node = q.front();  
-            os << node->val << " ";  
-            q.pop();  
+    while (q.empty() == false) {
+        int count = q.size();
+        while (count > 0) {
+            TreeNode* node = q.front();
+            os << node->val << " ";
+            q.pop();
             if(node->left != NULL){
                 q.push(node->left);
-            } 
-            if(node->right != NULL){ 
+            }
+            if(node->right != NULL){
                 q.push(node->right);
             }
             count--;
-        }  
+        }
         os << "\n";
-    }  
+    }
     return os;
 }
+
 template <typename T, typename U>
 std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
     os << "[" << p.first << ", " << p.second << "]";
---
+++ 110_Balanced_Binary_Tree/test01.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <queue>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+std::ostream& operator<<(std::ostream& os, TreeNode *root)  {  
+    if (root == NULL) return os;
+    std::queue<TreeNode*> q;
+ 
+    q.push(root);
+    while (q.empty() == false) {
+        int count = q.size();
+        while (count > 0) {
+            TreeNode* node = q.front();
+            os << node->val << " ";
+            q.pop();
+            if(node->left != NULL){
+                q.push(node->left);
+            }
+            if(node->right != NULL){
+                q.push(node->right);
+            }
+            count--;
+        }
+        os << "\n";
+    }
+    return os;
+}
+
+
+int depthOfTree(TreeNode* root, int depth){
+    if(root == NULL) return 0;
+    int ldepth = depthOfTree(root->left, depth) + 1;
+    int rdepth = depthOfTree(root->right, depth) + 1;
+    return std::max(ldepth, rdepth);
+}
+
+bool isBalanced(TreeNode* root){
+    if(root == NULL) return true;
+    if(abs(depthOfTree(root->left, 0) - depthOfTree(root->right, 0)) > 1){
+        return false;
+    }
+
+    if(!isBalanced(root->left) || !isBalanced(root->right)){
+        return false;
+    }
+    return true;
+}
+
+int main(int argc, char *argv[]){
+
+}
