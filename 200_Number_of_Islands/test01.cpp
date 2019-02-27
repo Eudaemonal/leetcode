5c30f345d1bf9c6326949be6416c93a00e7a2ff2
eudaemonal <eudaemonal@gmail.com>
2019-02-27 16:04:32 +1100

    Add course schedule

--- 200_Number_of_Islands/test01.cpp
+++ 200_Number_of_Islands/test01.cpp
@@ -12,23 +12,99 @@ std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>> &v)
 	return os;
 }
 
-int numIslands(vector<vector<char>>& grid) {
+class UnionFind{
+public:
+    UnionFind(int n){
+        count = n;
+        id.resize(n);
+        rank.resize(n);
+        for(int i = 0; i < n; ++i){
+            id[i] = i;
+        }
+    }
+
+    int find(int p){
+        while(p != id[p]){
+            id[p] = id[id[p]];
+            p = id[p];
+        }
+        return p;
+    }
+
+    int getCount(){
+        return count;
+    }
+
+    bool connected(int p, int q){
+        return find(p) == find(q);
+    }
+
+    void connect(int p, int q){
+        int i = find(p);
+        int j = find(q);
+
+        if(i == j) return;
+        if(rank[i] < rank[j]) id[i] = j;
+        else if(rank[i] > rank[j]) id[j] = i;
+        else{
+            id[j] = i;
+            rank[i]++;
+        }
+        count--;
+    }
+
+private:
+    std::vector<int> id;
+    std::vector<int> rank;
+    int count;
+};
+
+
+int numIslands(std::vector<std::vector<char>>& grid) {
 	int n = grid.size();
+    if(n == 0) return 0;
 	int m = grid[0].size();
     
+    UnionFind uf(n*m+1);
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < m; ++j){
+            if(grid[i][j] == '1'){
+                if(i-1 >= 0 && grid[i-1][j] == '1') uf.connect(i*m+j, (i-1)*m+j);
+                if(i+1 < n &&  grid[i+1][j] == '1') uf.connect(i*m+j, (i+1)*m+j);
+                if(j-1 >= 0 && grid[i][j-1] == '1') uf.connect(i*m+j, i*m+j-1);
+                if(j+1 < m &&  grid[i][j+1] == '1') uf.connect(i*m+j, i*m+j+1);
+            }
+        }
+    }
 
+    std::vector<int> count(n*m);
+    int res = 0;
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < m; ++j){
+            if(grid[i][j] == '1'){
+                int k = uf.find(i*m + j);
+                if(count[k] == 0){
+                    res++;
+                    count[k]++;
+                } else{
+                    count[k]++;
+                }
+            }
+        }
+    }
+
+    return res;
 }
 
 int main(int argc, char *argv[]){
 	int n, m;
 	std::cin >> n >> m;
-	std::vector<std::vector<int>> v(n, std::vector<int>(m));
+	std::vector<std::vector<char>> v(n, std::vector<char>(m));
 	for(int i = 0; i < n; ++i){
 		for(int j = 0; j < m; ++j){
 			std::cin >> v[i][j];
 		}
 	}
-	
-	std::cout << f(v) << "\n";
+	std::cout << numIslands(v) << "\n";
 	return 0;
 }
---
+++ 207_Course_Schedule/test01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    os << "[" << p.first << ", " << p.second << "]";
+    return os;
+}
+
+template <template <typename, typename...> class ContainerType, 
+          typename ValueType, typename... Args>
+std::ostream& operator<<(std::ostream& os, const ContainerType<ValueType, Args...>& c){
+    for(const auto& v : c){
+        os << v << ' ';
+    }
+    os << '\n';
+    return os;
+}
+
+bool canFinish(int numCourses, std::vector<std::pair<int, int>>& prerequisites) {
+    
+}
+
+int main(int argc, char *argv[]){
+    int n;
+    std::cin >> n;
+    std::vector<int> v(n);
+    for(int i = 0; i < n; ++i){
+        std::cin >> v[i];
+    }
+
+    std::cout << f(v) << "\n";
+    return 0;
+}
