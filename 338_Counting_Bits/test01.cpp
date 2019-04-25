8e01a321d9af758cdc4457227f0e1f6f858c2d26
eudaemonal <eudaemonal@gmail.com>
2019-04-25 15:24:36 +1000

    Add counting bits

---
+++ 338_Counting_Bits/test01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    os << "[" << p.first << ", " << p.second << "]";
+    return os;
+}
+
+template <template <typename, typename...> class ContainerType, 
+          typename ValueType, typename... Args>
+std::ostream& operator<<(std::ostream& os, const ContainerType<ValueType, Args...>& c){
+    for(const auto& v : c){
+        os << v << ' ';
+    }
+    os << '\n';
+    return os;
+}
+
+std::vector<int> countBits(int num) {
+    
+}
+
+int main(int argc, char *argv[]){
+    int n;
+    std::cin >> n;
+
+    std::cout << countBits(n) << "\n";
+    return 0;
+}
