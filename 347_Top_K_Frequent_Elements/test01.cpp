29cd3b4c28ea3d1cb29d83373256f37b72ad9369
eudaemonal <eudaemonal@gmail.com>
2019-04-29 20:55:17 +1000

    update

--- 347_Top_K_Frequent_Elements/test01.cpp
+++ 347_Top_K_Frequent_Elements/test01.cpp
@@ -17,19 +17,21 @@ std::ostream& operator<<(std::ostream& os, const ContainerType<ValueType, Args..
     return os;
 }
 
+std::vector<int> topKFrequent(std::vector<int>& nums, int k){
 
-std::vector<int> countBits(int num) {
-    std::vector<int> res(num+1, 0);
-    for(int i =1; i <= num; i++){
-        res[i] = res[i/2] + (i & 0x1);
-    }
-    return res;
 }
 
-int main(int argc, char *argv[]){
-    int n;
-    std::cin >> n;
 
-    std::cout << countBits(n);
+
+int main(int argc, char *argv[]){
+    int k, n;
+    std::cin >> k;
+    std::cin >> n;
+    std::vector<int> v(n);
+    for(int i = 0; i < n; ++i){
+        std::cin >> v[i];
+    }
+
+    std::cout << topKFrequent(v) << "\n";
     return 0;
 }
