da75e223c7e082ff90d574ad01a5e43612f8691d
eudaemonal <eudaemonal@gmail.com>
2019-05-29 15:50:23 +1000

    update solutions

--- 373_Find_K_Pairs_with_Smallest_Sums/test01.cpp
+++ 373_Find_K_Pairs_with_Smallest_Sums/test01.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <queue>
 
 template <typename T, typename U>
 std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
@@ -19,17 +21,37 @@ std::ostream& operator<<(std::ostream& os, const ContainerType<ValueType, Args..
 
 std::vector<std::vector<int>> 
 kSmallestPairs(std::vector<int>& nums1, std::vector<int>& nums2, int k) {
-    
+    std::vector<std::vector<int>> v;
+    if(nums1.empty() || nums2.empty()) return v;
+    auto cmp = [&nums1, &nums2](const std::pair<int, int>& a, const std::pair<int, int>& b){
+        return nums1[a.first] + nums2[a.second] > nums1[b.first] + nums2[b.second];
+    };
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, decltype(cmp)> minHeap(cmp);
+    minHeap.emplace(0, 0);
+    while(minHeap.size() && k--){
+        auto t = minHeap.top();
+        minHeap.pop();
+        v.push_back(std::vector<int>{nums1[t.first], nums2[t.second]});
+        if(t.first < nums1.size() - 1) minHeap.emplace(t.first+1, t.second);
+        if(t.first == 0 && t.second < nums2.size() - 1) minHeap.emplace(t.first, t.second+1);
+    }
+    return v;
 }
 
 int main(int argc, char *argv[]){
-    int n;
+    int k, n;
+    std::cin >> k;
     std::cin >> n;
-    std::vector<int> v(n);
+    std::vector<int> v1(n);
     for(int i = 0; i < n; ++i){
-        std::cin >> v[i];
+        std::cin >> v1[i];
+    }
+    std::cin >> n;
+    std::vector<int> v2(n);
+    for(int i = 0; i < n; ++i){
+        std::cin >> v2[i];
     }
 
-    std::cout << f(v) << "\n";
+    std::cout << kSmallestPairs(v1, v2, k);
     return 0;
 }
